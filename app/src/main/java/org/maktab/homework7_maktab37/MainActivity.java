ab8f79971aab8b56746a4c0985e94dd1c187425a
SanaNazariNezhad <sana.nazari1995@gmail.com>
2020-08-21 16:11:44 +0430

    revisit.

--- app/src/main/java/org/maktab/homework7_maktab37/MainActivity.java
+++ app/src/main/java/org/maktab/homework7_maktab37/MainActivity.java
@@ -8,134 +8,63 @@ import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView mRed;
-    private TextView mOrange;
-    private TextView mYellow;
-    private TextView mGreen;
-    private TextView mBlue;
-    private TextView mIndigo;
-    private TextView mViolet;
-    private int color;
 
-    private Handler mHandler = new Handler();
+    private TextView[] views;
+
+    int[] mViewIds = {R.id.red_txt, R.id.orange_txt, R.id.yellow_txt, R.id.green_txt, R.id.blue_txt, R.id.indigo_txt, R.id.violet_txt};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        views = new TextView[7];
         findViews();
         clickListeners();
     }
 
     private void findViews() {
-        mRed = findViewById(R.id.red_txt);
-        mOrange = findViewById(R.id.orange_txt);
-        mYellow = findViewById(R.id.yellow_txt);
-        mGreen = findViewById(R.id.green_txt);
-        mBlue = findViewById(R.id.blue_txt);
-        mIndigo = findViewById(R.id.indigo_txt);
-        mViolet = findViewById(R.id.violet_txt);
+        views[0] = findViewById(R.id.red_txt);
+        views[1] = findViewById(R.id.orange_txt);
+        views[2] = findViewById(R.id.yellow_txt);
+        views[3] = findViewById(R.id.green_txt);
+        views[4] = findViewById(R.id.blue_txt);
+        views[5] = findViewById(R.id.indigo_txt);
+        views[6] = findViewById(R.id.violet_txt);
     }
 
     private void clickListeners() {
-        mRed.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mRed.setVisibility(View.GONE);
-                color = getColor(R.color.red);
-                setColors(color);
-                mHandler.postDelayed(myRunnable,5000);
+        for (int id : mViewIds) {
+            final TextView textView = findViewById(id);
 
-            }
-        });
-        mOrange.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mOrange.setVisibility(View.GONE);
-                color = getColor(R.color.orange);
-                setColors(color);
-                mHandler.postDelayed(myRunnable,5000);
+            textView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    for (TextView txt : views) {
+                        textView.setVisibility(View.GONE);
+                        txt.setText(textView.getText());
+                        view.postDelayed(myRunnable, 5000);
+                    }
 
-            }
-        });
-        mYellow.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mYellow.setVisibility(View.GONE);
-                color = getColor(R.color.yellow);
-                setColors(color);
-                mHandler.postDelayed(myRunnable,5000);
-
-            }
-        });
-        mGreen.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mGreen.setVisibility(View.GONE);
-                color = getColor(R.color.green);
-                setColors(color);
-                mHandler.postDelayed(myRunnable,5000);
-
-            }
-        });mBlue.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mBlue.setVisibility(View.GONE);
-                color = getColor(R.color.blue);
-                setColors(color);
-                mHandler.postDelayed(myRunnable,5000);
-
-            }
-        });mIndigo.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mIndigo.setVisibility(View.GONE);
-                color = getColor(R.color.indigo);
-                setColors(color);
-                mHandler.postDelayed(myRunnable,5000);
-
-            }
-        });
-        mViolet.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mViolet.setVisibility(View.GONE);
-                color = getColor(R.color.violet);
-                setColors(color);
-                mHandler.postDelayed(myRunnable,5000);
-
-            }
-        });
-
-    }
-
-    public void defaultColor() {
-        mRed.setBackgroundColor(getColor(R.color.red));
-        mOrange.setBackgroundColor(getColor(R.color.orange));
-        mYellow.setBackgroundColor(getColor(R.color.yellow));
-        mGreen.setBackgroundColor(getColor(R.color.green));
-        mBlue.setBackgroundColor(getColor(R.color.blue));
-        mIndigo.setBackgroundColor(getColor(R.color.indigo));
-        mViolet.setBackgroundColor(getColor(R.color.violet));
-    }
-
-    public void setColors(int color) {
-        mRed.setBackgroundColor(color);
-        mOrange.setBackgroundColor(color);
-        mYellow.setBackgroundColor(color);
-        mGreen.setBackgroundColor(color);
-        mBlue.setBackgroundColor(color);
-        mIndigo.setBackgroundColor(color);
-        mViolet.setBackgroundColor(color);
-    }
-
-    Runnable myRunnable = new  Runnable() {
-        @Override
-        public void run() {
-            defaultColor();
-
+                }
+            });
         }
     }
 
-    ;
+    public void defaultText() {
+        views[0].setText(R.string.red_txt_name);
+        views[1].setText(R.string.orange_txt_name);
+        views[2].setText(R.string.yellow_txt_name);
+        views[3].setText(R.string.green_txt_name);
+        views[4].setText(R.string.blue_txt_name);
+        views[5].setText(R.string.indigo_txt_name);
+        views[6].setText(R.string.violet_txt_name);
+    }
+
+    Runnable myRunnable = new Runnable() {
+        @Override
+        public void run() {
+            defaultText();
+
+        }
+    };
 }
